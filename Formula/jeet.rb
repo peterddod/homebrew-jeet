5a51f5da7454ff0d45da2cfdb8e6587c65c032ed
Peter William Cairns Dodd <peterdodd01@gmail.com>
2026-05-27 16:22:12 +0100

    Update jeet to 0.2.5

--- Formula/jeet.rb
+++ Formula/jeet.rb
@@ -4,29 +4,29 @@
 class Jeet < Formula
   desc "Global git repo index and worktree manager"
   homepage "https://github.com/peterddod/jeet"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
   head "https://github.com/peterddod/jeet.git", branch: "main"
 
   on_macos do
     on_arm do
-      url "https://github.com/peterddod/jeet/releases/download/v0.2.4/jeet-v0.2.4-aarch64-apple-darwin.tar.gz"
-      sha256 "30ffd1a9ed61b3a4172e619fa4875060df5c73d93f6d8415cb3c816e21302dea"
+      url "https://github.com/peterddod/jeet/releases/download/v0.2.5/jeet-v0.2.5-aarch64-apple-darwin.tar.gz"
+      sha256 "3567dcac51c66241424ec5886fe85611c6d7c859bbee5e778b6872b89877a326"
     end
     on_intel do
-      url "https://github.com/peterddod/jeet/releases/download/v0.2.4/jeet-v0.2.4-x86_64-apple-darwin.tar.gz"
-      sha256 "ceeafe3489047729c4ab0384678bfbc953faea0356e7f320e48bdf2d44bd6b41"
+      url "https://github.com/peterddod/jeet/releases/download/v0.2.5/jeet-v0.2.5-x86_64-apple-darwin.tar.gz"
+      sha256 "9a4b762772ba1ceca3e9b678915c04c74ab95d5f82dc80f29006336ae62c8304"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/peterddod/jeet/releases/download/v0.2.4/jeet-v0.2.4-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "451fb309ac2fa4e9c39528a00b18614df7fcca2baebd00178e55c747403b6f62"
+      url "https://github.com/peterddod/jeet/releases/download/v0.2.5/jeet-v0.2.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "05c9ecf832f0bc6f21a8038a1f4cd5b0e2ac5e2139325c3db4f049e4c65c3c19"
     end
     on_intel do
-      url "https://github.com/peterddod/jeet/releases/download/v0.2.4/jeet-v0.2.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e1da270c73a98dd90e1d73028db9f4fab60246482be2ad5bc7543d4ddbd74245"
+      url "https://github.com/peterddod/jeet/releases/download/v0.2.5/jeet-v0.2.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "85aaff477b7d420f8415d7ff6eb40100cb70fe0452b83f1778db031fdda0d9a7"
     end
   end
 
